0140965be6483c1f031f09ceea8c0b9c1347f2a7
Brian Takita <brian.takita@gmail.com>
2022-10-27 23:50:45 -0400

    @mckenzie_starlite__camper/bumper: bumper: run index.ts
    git lfs track bumper/*

--- libs/bumper/bin/bumper/index.sh
+++ libs/bumper/bin/bumper/index.sh
@@ -1,4 +1,6 @@
 export CWD=$(pwd)
-cd $(dirname $0)/../..
+DIR=$(dirname $0)
+cd $DIR/../..
 ./node_modules/.bin/vite build
-node -e "import('./lib/index.js').then(({ Svg_Bumper__run })=>Svg_Bumper__run())" ./lib/index.js
+cd $DIR
+tsx ./index.ts "$@"
---
+++ libs/bumper/bin/bumper/index.ts
@@ -0,0 +1,2 @@
+import { Svg_Bumper__run } from '../../lib/index.js'
+Svg_Bumper__run().then()
