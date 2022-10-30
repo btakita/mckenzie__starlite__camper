5a9b2cd922edc4054254f79c27bfd1f1ba72d681
Brian Takita <brian.takita@gmail.com>
2022-10-30 00:00:30 -0400

    Svg__jscad__bumper: refactor: + back__bumper__assembly__svg_,bumper__assembly__jscad_a_,project_colorize

--- libs/bumper/src/Svg__jscad__bumper.tsx
+++ libs/bumper/src/Svg__jscad__bumper.tsx
@@ -1,5 +1,10 @@
 import { _p_ } from '@ctx-core/function'
 import modeling from '@jscad/modeling'
+import type { RGB, RGBA } from '@jscad/modeling/src/colors'
+import type { Geom2 } from '@jscad/modeling/src/geometries/geom2'
+import type { Geom3 } from '@jscad/modeling/src/geometries/geom3'
+import type { Colored } from '@jscad/modeling/src/geometries/types'
+import { ProjectOptions } from '@jscad/modeling/src/operations/extrusions/project'
 import svg_serializer from '@jscad/svg-serializer'
 import { XMLParser } from 'fast-xml-parser'
 import { type VoidProps } from 'solid-js'
@@ -28,7 +33,16 @@ import {
 	receiver__width__in,
 	receivers__back__width__in
 } from './_lib.js'
-export function Svg__jscad__bumper($p:VoidProps) {
+const { project } = modeling.extrusions
+const { union } = modeling.booleans
+const { colorize, cssColors } = modeling.colors
+const { gray } = cssColors
+const { cuboid } = modeling.primitives
+const back__project__options:ProjectOptions = {
+	axis: [0, -1, 0],
+	origin: [0, -1, 0]
+}
+export function Svg__jscad__bumper() {
 	return (
 		<svg xmlns="http://www.w3.org/2000/svg" class="doc" width={in_s_(doc__width__in)} height={in_s_(doc__height__in)}>
 			<C__page/>
@@ -58,75 +72,66 @@ export function C__page($p:VoidProps<{ y?:number|string }>) {
 				data-back__width__in={back__width__in}
 				data-back__height__in={back__height__in}
 				viewBox={`0 0 ${in__px_(scene__width__in)} ${in__px_(scene__height__in)}`}
-				innerHTML={back__bumper__assembly__svg_().split('\n').slice(3)}
+				innerHTML={back__bumper__svg_a.join('\n')}
 			/>
 		</svg>
 	)
 }
-export function back__bumper__assembly__svg_() {
-	const { colorize } = modeling.colors
-	const { project } = modeling.extrusions
-	const back__bumper__jscad = bumper__assembly__jscad_()
-	const svg_a = svg_serializer.serialize(
+export function back__bumper__assembly__svg_():string {
+	return svg_serializer.serialize(
 		{ unit: 'in' },
-		_p_(
-			back__bumper__jscad,
-			$=>project({
-				axis: [0, -1, 0],
-				origin: [0, -1, 0]
-			}, $),
-			$=>colorize(back__bumper__jscad.color, $),
-		))
-	return svg_a[0]
+		bumper__assembly__jscad_a_().map($=>
+			project_colorize(back__project__options, $))
+	)[0]
 }
-export function bumper__assembly__jscad_() {
-	const { union } = modeling.booleans
-	const { colorize, cssColors } = modeling.colors
-	const { gray } = cssColors
-	const { cuboid } = modeling.primitives
+export function bumper__assembly__jscad_a_():(Geom3&Colored)[] {
+	return [
+		frame__jscad_(),
+		body__jscad_(),
+	]
+}
+function frame__jscad_():Geom3&Colored {
 	return _p_(
 		union(
-			frame__jscad_(),
-			body__jscad_(),
+			frame__driver__jscad_(),
+			frame__passenger__jscad_(),
 		),
-		$=>colorize(gray, $)
+		$=>colorize(gray, $),
 	)
-	function frame__jscad_() {
-		return _p_(
-			union(
-				frame__driver__jscad_(),
-				frame__passenger__jscad_(),
-			),
-			$=>colorize(gray, $),
-		)
-		function frame__driver__jscad_() {
-			return cuboid({
-				center: [
-					back__receivers__offset__x__in + receiver__width__in / 2,
-					frame__z__in + frame__depth__in / 2,
-					frame__ground__in + receiver__height__in / 2],
-				size: [receiver__width__in, frame__depth__in, receiver__height__in]
-			})
-		}
-		function frame__passenger__jscad_() {
-			return cuboid({
-				center: [
-					back__receivers__offset__x__in + receivers__back__width__in - receiver__width__in / 2,
-					frame__z__in + frame__depth__in / 2,
-					frame__ground__in + receiver__height__in / 2],
-				size: [receiver__width__in, frame__depth__in, receiver__height__in]
-			})
-		}
+	function frame__driver__jscad_():Geom3 {
+		return cuboid({
+			center: [
+				back__receivers__offset__x__in + receiver__width__in / 2,
+				frame__z__in + frame__depth__in / 2,
+				frame__ground__in + receiver__height__in / 2],
+			size: [receiver__width__in, frame__depth__in, receiver__height__in]
+		})
 	}
-	function body__jscad_() {
-		return _p_(
-			cuboid({
-				center: [
-					body__width__in / 2,
-					body__depth__in / 2,
-					frame__ground__in + receiver__height__in + frame__body__gap__in + body__height__in / 2],
-				size: [body__width__in, body__depth__in, body__height__in]
-			}),
-			$=>colorize(gray, $))
+	function frame__passenger__jscad_():Geom3 {
+		return cuboid({
+			center: [
+				back__receivers__offset__x__in + receivers__back__width__in - receiver__width__in / 2,
+				frame__z__in + frame__depth__in / 2,
+				frame__ground__in + receiver__height__in / 2],
+			size: [receiver__width__in, frame__depth__in, receiver__height__in]
+		})
 	}
+}
+function body__jscad_():Geom3&Colored {
+	return _p_(
+		cuboid({
+			center: [
+				body__width__in / 2,
+				body__depth__in / 2,
+				frame__ground__in + receiver__height__in + frame__body__gap__in + body__height__in / 2],
+			size: [body__width__in, body__depth__in, body__height__in]
+		}),
+		$=>colorize(gray, $))
+}
+function project_colorize(
+	options:ProjectOptions, $:Geom3, color:RGB|RGBA = $.color
+):Geom2&Colored {
+	return _p_($,
+		$=>project(options, $),
+		$=>colorize(color, $))
 }
